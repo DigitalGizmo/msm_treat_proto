87898765dfe3ce123da41d895c38896b0eb9456d
Don Button <donpublic@digitalgizmo.com>
2019-08-30 16:20:49 -0400

    mid trans to single auto layer. works

--- js/map-vue.js
+++ js/map-vue.js
@@ -5,29 +5,33 @@ new Vue({
 	data: { 
 		map: null,
 		tileLayer: null,
+		markerList: null,
 		layers: [
 			{
 				id: 0,
-				name: 'Sites of Entries',
+				name: 'Primary Entries',
 				active: false,
 				features: [
 					{
 					  id: 0,
 					  name: 'amherst-college',
 					  type: 'marker',
-					  coords: [42.371013, -72.50],
+					  latitude: 42.371013,
+					  longitude: -72.50,
 					},
 					{
 					  id: 1,
 					  name: 'residence-hitchcocks-amherst',
 					  type: 'marker',
-					  coords: [42.368709, -72.55],
+					  latitude: 42.368709,
+					  longitude: -72.55,
 					},
 					{
 					  id: 2,
 					  name: 'appleton-cabinet',
 					  type: 'marker',
-					  coords: [42.371505, -72.6],
+					  latitude: 42.371505,
+					  longitude: -72.6,
 					},
 				],
 			},
@@ -55,16 +59,52 @@ new Vue({
 			this.tileLayer.addTo(this.map);
 		},
 		initLayers() {
-			this.layers.forEach((layer) => {
-				// Initialize the layer
-				const markerFeatures = layer.features.filter(feature => feature.type === 'marker');
 
-				markerFeatures.forEach((feature) => {
-				  feature.leafletObject = L.marker(feature.coords)
-				    .bindPopup(feature.name);
-				});
+			// // Old-school:
+			// var a2 = a.map(function(s){ return s.length });
+
+			// // ECMAscript 6 using arrow functions
+			// var a3 = a.map( s => s.length );
+
+
+			this.markerList = [];
+
+			// this.layers[0].features.forEach((feature) => {
+			this.layers[0].features.forEach(function(feature) {
+
+				console.log(" - lat: " + feature.latitude); // feature.coords
+				feature.leafletObject = L.marker([feature.latitude, feature.longitude])
+					.bindPopup(feature.name);
+				// // feature.leafletObject.addTo(this.map);
+
+				// // This is the direct, non-array way.
+				// L.marker([feature.latitude, 
+				// 	feature.longitude]).addTo(this.map).on("click", function(e) {
+				// 	  // goNext("holy");
+				// 	  console.log(" -- clicked marker");
+				// 	});
+
+
+
+				// this.markerList.push(L.marker([feature.latitude, 
+				// 	 feature.longitude]).addTo(this.map).on("click", function(e) { 
+				// 	 	// goNext("holy");
+				// 	 	console.log(" - marker index: " + i);
+				// 	 	// v-on:click = "incrementEntry('next')"
+
+				// 	 	// Prop need all code in vue for this to work
+				// 	 	// jrnEntry.setEntry(i, 10);
+
+				// 	 	// Temporarily this works, prob because it's not defined until afer siteListJson is defined
+				// 	 	// jrnEntry.incrementEntry('next');
+
+				// 	 })
+					
+				// );
 
 			});
+			// siteMarkers = L.layerGroup(markerList);
+			// siteMarkers.addTo(treatmap);
 
 		},
 		layerChanged(layerId, active) { 
