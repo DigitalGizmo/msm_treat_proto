7606d4597b20733719d92a67436bd7fc3834f23b
Don Button <donpublic@digitalgizmo.com>
2019-09-13 12:31:06 -0400

    add 5 Juliet locations

--- js/treat-map.js
+++ js/treat-map.js
@@ -7,8 +7,8 @@ Vue.component('journal-item', {
 	template: `
 		<div class="map-scroll">
 			<p><strong>{{ anentry.title }} </strong> </p>
-			<img v-bind:src="'images/menupics/' + anentry.name + '.jpg'" />
-			<span v-html="anentry.map_blurb"></span>
+			<img v-bind:src="'images/menupics/' + anentry.shortName + '.jpg'" />
+			<span v-html="anentry.jrnText"></span>
 		</div>
 	`
 })
@@ -27,13 +27,15 @@ var mapApp = new Vue({
 		roadsShowing: false,
 		// need non-null intialization. Will be overwritten by 1st real data via initContent
 		entry: {
-					  id: 0,
-					  name: 'amherst-college',
-					  type: 'marker',
+					  ordinal: 0,
+					  shortName: 'amherst-college',
+					  zoomLevel: 10,
 					  title: "Amherst College",
-					  latitude: 44,
-					  longitude: -69,
-					  map_blurb: "<p>[vue-data init - placeholder]</p>",
+					  lat: 44,
+					  lon: -69,
+					  month: 'September',
+					  day: '',
+					  jrnText: "<p>[vue-data init - placeholder]</p>",
 					},
 		layers: [
 			{
@@ -42,40 +44,105 @@ var mapApp = new Vue({
 				active: false,
 				features: [
 					{
-					  id: 0,
-					  name: 'appleton-cabinet',
-					  type: 'marker',
+					  ordinal: 0,
+					  shortName: 'appleton-cabinet',
+					  zoomLevel: 10,
 					  title: "Start - Bangor",
-					  latitude: 44.7971185,
-					  longitude: -68.767824,
-					  map_blurb: "<p>[vue-data]: Appleton Cabinet opened in 1855. It was constructed specifically to house the college&apos;s natural history collection, including Edward Hitchcock&apos;s collection of fossil &quot;bird tracks,&quot; now identified as dinosaur footprints. The building was later converted for academic use and currently is a dormitory. The beautiful geological collections are at the Beneski Museum, just down the hill on campus.</p>"
+					  lat: 44.7971185,
+					  lon: -68.767824,
+					  month: 'September',
+					  day: '',
+					  jrnText: "<p>[vue-data]: Appleton Cabinet opened in 1855. It was constructed specifically to house the college&apos;s natural history collection, including Edward Hitchcock&apos;s collection of fossil &quot;bird tracks,&quot; now identified as dinosaur footprints. The building was later converted for academic use and currently is a dormitory. The beautiful geological collections are at the Beneski Museum, just down the hill on campus.</p>"
 					},
 					{
-					  id: 6,
-					  name: 'day-two',
-					  type: 'marker',
+					  ordinal: 2,
+					  shortName: 'day-two',
+					  zoomLevel: 10,
 					  title: "First stop",
-					  latitude: 44.880163,
-					  longitude:  -68.659326,
-					  map_blurb: "<p>[Don't see a note on whether this is the same day, Sunday, as the first entry.]</p>",
+					  lat: 44.880163,
+					  lon:  -68.659326,
+					  month: 'September',
+					  day: '',
+					  jrnText: "<p>[Don't see a note on whether this is the same day, Sunday, as the first entry.]</p>",
 					},
 					{
-					  id: 10,
-					  name: 'amherst-college',
-					  type: 'marker',
+					  ordinal: 70,
+					  shortName: 'abol-falls',
+					  zoomLevel: 10,
+					  title: "Abolosokmasesick Falls",
+					  lat: 45.82655,
+					  lon:  -68.96821,
+					  month: 'October',
+					  day: '9',
+					  jrnText: "<p>Lorem ipsum dolor sit amet consectetur adipiscing elit, semper nascetur phasellus himenaeos quis commodo, fermentum nisl elementum nunc etiam diam. Penatibus feugiat potenti ad interdum curae sodales hendrerit sociis eros semper, nibh auctor fermentum senectus ultrices ligula class fringilla sociosqu nunc tellus, aliquet magnis mattis lacus cum pretium praesent curabitur facilisis. Donec justo porttitor lacinia arcu ligula venenatis posuere erat pretium mauris sollicitudin per blandit congue convallis, tincidunt odio mattis tempor sagittis fames molestie nostra praesent pellentesque pulvinar primis lectus.<p></p> Suspendisse eget tellus justo hac ante nisl massa nam maecenas, lectus mus duis mi nullam porttitor habitasse rutrum torquent praesent, ac venenatis sociosqu montes nibh ad quisque suscipit. Tortor hendrerit fringilla lobortis penatibus aliquam varius nibh tempor scelerisque ante, bibendum duis curae venenatis porta suscipit leo luctus vulputate velit enim, blandit netus justo at cubilia hac senectus dui facilisi. Gravida congue dapibus feugiat nam dictum mollis nostra cursus, metus augue tempus ad sollicitudin curabitur sociosqu, felis lobortis praesent aptent erat netus interdum.</p>",
+					},
+					{
+					  ordinal: 72,
+					  shortName: 'neso-falls',
+					  zoomLevel: 11,
+					  title: "Nesowadnehunk Falls",
+					  lat: 45.84633, 
+					  lon:  -69.03056,
+					  month: 'October',
+					  day: '10',
+					  jrnText: "<p>Lorem ipsum dolor sit amet consectetur adipiscing elit, semper nascetur phasellus himenaeos quis commodo, fermentum nisl elementum nunc etiam diam. Penatibus feugiat potenti ad interdum curae sodales hendrerit sociis eros semper, nibh auctor fermentum senectus ultrices ligula class fringilla sociosqu nunc tellus, aliquet magnis mattis lacus cum pretium praesent curabitur facilisis. Donec justo porttitor lacinia arcu ligula venenatis posuere erat pretium mauris sollicitudin per blandit congue convallis, tincidunt odio mattis tempor sagittis fames molestie nostra praesent pellentesque pulvinar primis lectus.<p></p> Suspendisse eget tellus justo hac ante nisl massa nam maecenas, lectus mus duis mi nullam porttitor habitasse rutrum torquent praesent, ac venenatis sociosqu montes nibh ad quisque suscipit. Tortor hendrerit fringilla lobortis penatibus aliquam varius nibh tempor scelerisque ante, bibendum duis curae venenatis porta suscipit leo luctus vulputate velit enim, blandit netus justo at cubilia hac senectus dui facilisi. Gravida congue dapibus feugiat nam dictum mollis nostra cursus, metus augue tempus ad sollicitudin curabitur sociosqu, felis lobortis praesent aptent erat netus interdum. </p>",
+					},
+					{
+					  ordinal: 74,
+					  shortName: 'neso-camp',
+					  zoomLevel: 11,
+					  title: "Remain in Camp",
+					  lat: 45.84615, 
+					  lon:  -69.03522,
+					  month: 'October',
+					  day: '11',
+					  jrnText: "<p>Lorem ipsum dolor sit amet consectetur adipiscing elit, semper nascetur phasellus himenaeos quis commodo, fermentum nisl elementum nunc etiam diam. Penatibus feugiat potenti ad interdum curae sodales hendrerit sociis </p>",
+					},
+					{
+					  ordinal: 80,
+					  shortName: 'chee-inlet',
+					  zoomLevel: 10,
+					  title: "Cheesuncook Inlet",
+					  lat: 46.052, 
+					  lon:  -69.34847,
+					  month: 'October',
+					  day: '14',
+					  jrnText: "<p>Lorem ipsum dolor sit amet consectetur adipiscing elit, semper nascetur phasellus himenaeos quis commodo, fermentum nisl elementum nunc etiam diam. Penatibus feugiat potenti ad interdum curae sodales hendrerit sociis eros semper, nibh auctor fermentum senectus ultrices ligula class fringilla sociosqu nunc tellus, aliquet magnis mattis lacus cum pretium praesent curabitur facilisis. Donec justo porttitor lacinia arcu ligula venenatis posuere erat pretium mauris sollicitudin per blandit congue convallis, tincidunt odio mattis tempor sagittis fames molestie nostra praesent pellentesque pulvinar primis lectus.<p></p> Suspendisse eget tellus justo hac ante nisl massa nam maecenas, lectus mus duis mi nullam porttitor habitasse rutrum torquent praesent, ac venenatis sociosqu montes nibh ad quisque suscipit. Tortor hendrerit fringilla lobortis penatibus aliquam varius nibh tempor scelerisque ante, bibendum duis curae venenatis porta suscipit leo luctus vulputate velit enim, blandit netus justo at cubilia hac senectus dui facilisi. Gravida congue dapibus feugiat nam dictum mollis nostra cursus, metus augue tempus ad sollicitudin curabitur sociosqu, felis lobortis praesent aptent erat netus interdum. </p>",
+					},
+					{
+					  ordinal: 82,
+					  shortName: 'umba-pond',
+					  zoomLevel: 12,
+					  title: "Umbazookskus Pond",
+					  lat: 46.14653, 
+					  lon:  -69.3497,
+					  month: 'October',
+					  day: '17',
+					  jrnText: "<p>? </p>",
+					},
+
+
+					{
+					  ordinal: 100,
+					  shortName: 'amherst-college',
+					  zoomLevel: 10,
 					  title: "Mt. Katahdin",
-					  latitude: 45.9005,
-					  longitude:  -68.9256,
-					  map_blurb: "<p>Amherst College was founded in 1821 and is the third oldest college in Massachusetts. Edward Hitchcock was its third president and donated his huge collection of fossil tracks to the college where it can be seen today.</p>",
+					  lat: 45.9005,
+					  lon:  -68.9256,
+					  month: 'October',
+					  day: '',
+					  jrnText: "<p>Amherst College was founded in 1821 and is the third oldest college in Massachusetts. Edward Hitchcock was its third president and donated his huge collection of fossil tracks to the college where it can be seen today.</p>",
 					},
 					{
-					  id: 2,
-					  name: 'residence-hitchcocks-amherst',
-					  type: 'marker',
+					  ordinal: 105,
+					  shortName: 'residence-hitchcocks-amherst',
+					  zoomLevel: 10,
 					  title: "Penobscot River",
-					  latitude: 45.9498,
-					  longitude: -69.1892,
-					  map_blurb: "<p>The Hitchcocks lived in this house on South Pleasant street in Amherst until Edward was elected president, and to this house they returned, after resigning the presidency, to spend the remainder of their lives. The house was at first a plain building with no wings and a small porch in front. Between 1836 and 1840 Edward built the octagon as a place to work and to display his private collection.</p>",
+					  lat: 45.9498,
+					  lon: -69.1892,
+					  month: 'October',
+					  day: '',
+					  jrnText: "<p>The Hitchcocks lived in this house on South Pleasant street in Amherst until Edward was elected president, and to this house they returned, after resigning the presidency, to spend the remainder of their lives. The house was at first a plain building with no wings and a small porch in front. Between 1836 and 1840 Edward built the octagon as a place to work and to display his private collection.</p>",
 					},
 				], // end features
 			}, // end first layer
@@ -92,8 +159,8 @@ var mapApp = new Vue({
 	methods: { 
 		initMap() {
 
-			this.map = L.map('mapdiv').setView([this.layers[0].features[0].latitude, 
-				this.layers[0].features[0].longitude], 8);
+			this.map = L.map('mapdiv').setView([this.layers[0].features[0].lat, 
+				this.layers[0].features[0].lon], 8);
 			// this.map = L.map('mapdiv').setView([42.0, -72.6], 9);
 			// // Base Layer
 			// this.baseLayer = L.tileLayer(
@@ -158,8 +225,8 @@ var mapApp = new Vue({
 			// Need to understand => and "this" better
 			this.layers[0].features.forEach((feature, index) => {
 			// this.layers[0].features.forEach(function(feature) {
-				// console.log(" - lat: " + feature.latitude); // feature.coords
-				feature.hamObject = L.marker([feature.latitude, feature.longitude])
+				// console.log(" - lat: " + feature.lat); // feature.coords
+				feature.hamObject = L.marker([feature.lat, feature.lon])
 					.on("click", function(e) { 
 					 	// console.log(" - marker name: " + feature.name);
 					 	// mapApp.setEntry(feature.id, 10);
@@ -172,7 +239,7 @@ var mapApp = new Vue({
 			// this.layers[0].features.forEach(function(feature) {
 			// Above doesn't work -- must be related to the "this" diff treatment
 			this.layers[0].features.forEach((feature) => {
-				// console.log(" -- fadd longitude: " + feature.longitude);
+				// console.log(" -- fadd lon: " + feature.lon);
 				feature.hamObject.addTo(this.map);
 			});
 		},
@@ -195,8 +262,8 @@ var mapApp = new Vue({
 			// Set the new map zoom location
 			// Had big problems here with "this"-- scope issues "myApp" works
 			mapApp.map.setView([
-				this.entry.latitude, 
-				this.entry.longitude
+				this.entry.lat, 
+				this.entry.lon
 				], zoomLevel) 
 		},
 		initContent: function() {
